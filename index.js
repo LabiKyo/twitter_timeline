9dfa1c8c060b002ef33a849bdc89b3da904a0387
Labi Kyo <labikyo@gmail.com>
2011-12-16 05:27:59 -0500

    make it work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var
   util = require('util')
   EventEmitter = require('events').EventEmitter
+  assert = require('assert')
 
   Twitter = require('twitter')
   mongo = require('mongoskin')
@@ -19,10 +20,24 @@ var twitter = new Twitter({
 var db = mongo.db('mongo://twitter:twitter@localhost/twitter');
 var tweets = db.collection('tweets');
 var record = db.collection('record');
+/*
+record.find({min: {$exists: true}}).toArray(function (err, docs) {
+  console.log(docs[0].min);
+  assert(false);
+  get_status_less_than();
+});
+*/
+record.findOne({min: {$exists: true}}, function (err, data) {
+  var min;
+  if (!data) {
+    min = undefined;
+  } else {
+    min = data.min;
+  }
+  get_status_less_than(min);
+});
 
 // start running
-get_status_less_than();
-
 function get_status_less_than(max_id) {
   console.log('it starts at max id = ', max_id);
   var min_id = Infinity;
@@ -44,7 +59,9 @@ function get_status_less_than(max_id) {
       promise.count = 0;
       promise.on('end', function () {
         console.log('it ends now! min id = ', min_id);
-        get_status_less_than(min_id);
+        record.update({min: {$exists: true}}, {$set: {min: min_id}}, {upsert: true}, function () {
+          get_status_less_than(min_id);
+        });
       });
       for (i in data) {
         var tweet = data[i];
